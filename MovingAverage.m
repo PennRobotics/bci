e7ff4d9d7d7cfee3d3fe6eaebd7557427742e484
Brian Wright <brwr@mit.edu>
2016-04-19 19:14:18 -0400

    Colossal pain in the ass

--- MovingAverage.m
+++ MovingAverage.m
@@ -1,19 +1,14 @@
 function [output] = MovingAverage(input)
-% TODO(brwr): Function averages "back in time"
 
-current_value = NaN;
-fn_len = size(input, 1);
+max_chan = size(input, 2);
+fn_len = size(input, 1) / 50;
 
-output = zeros(size(input));
+output = zeros(fn_len, max_chan);
 
-for chan = 1 : size(input, 2)
-  for i = 1 : fn_len - 99
-    if mod(i, 50) == 1
-      current_value = mean(input(i : i + 99, chan));
-    end
-    output(i, chan) = current_value;
-  end
-  for i = fn_len - 98 : fn_len 
+for chan = 1 : max_chan
+  output(1, chan) = 0;
+  for i = 2 : fn_len
+    current_value = mean(input(50*(i-2) + 1 : 50 * (i-2) + 100, chan));
     output(i, chan) = current_value;
   end
 end
--- MovingFreqDomain.m
+++ MovingFreqDomain.m
@@ -1,58 +1,48 @@
-function [output] = MovingAverage(input)
-
-debug_output = 1; %TODO
+function [output] = MovingFreqDomain(input)
 
 win_size = 100; % [ms]
 overlap = 50; % [ms]
-Fs = 200; % [freq. divisions]
+Fs = 200; % [freq. divisions * 2]
 F = 1000; % [Hz]
 
-fn_len = size(input, 1);
 max_chan = size(input, 2);
+fn_len = size(input, 1) / 50;
 
-outputA = zeros(max_chan, fn_len);
-outputB = zeros(max_chan, fn_len);
-outputC = zeros(max_chan, fn_len);
-outputD = zeros(max_chan, fn_len);
-outputE = zeros(max_chan, fn_len);
+outputA = zeros(fn_len, max_chan);
+outputB = zeros(fn_len, max_chan);
+outputC = zeros(fn_len, max_chan);
+outputD = zeros(fn_len, max_chan);
+outputE = zeros(fn_len, max_chan);
 
 % This is used to make frequency bands relatively same-sized
 reduce = (linspace(1e-4, 1, 101)' .^ 2) ./ linspace(1500,1,101)';
 
-if debug_output; fprintf('\n'); end
-
 for chan = 1 : max_chan
 
-  if debug_output; fprintf('%i ', chan); end
+  outputA(1, chan) = 0;
+  outputB(1, chan) = 0;
+  outputC(1, chan) = 0;
+  outputD(1, chan) = 0;
+  outputE(1, chan) = 0;
+
   % spectrogram(sub_1_train_part(:,chan), win_size, overlap, Fs, F);
   [S, F_, T_, P, Fc, Tc] = spectrogram(input(:,chan), win_size, overlap, Fs, F);
 
-  for i = 1 : fn_len - overlap
-    if mod(i, overlap) == 1
-      idx = ceil(i / overlap);
+  for i = 2 : fn_len
       % Constant also used to make frequency bands similar magnitude
-      freq_5_to_15    =  3.0 * mean(P( 2: 4, idx) .* reduce( 2: 4));
-      freq_20_to_25   =  5.5 * mean(P( 5: 6, idx) .* reduce( 5: 6));
-      freq_75_to_115  = 20.0 * mean(P(16:24, idx) .* reduce(16:24));
-      freq_125_to_160 = 29.5 * mean(P(26:33, idx) .* reduce(26:33));
-      freq_160_to_175 = 34.5 * mean(P(33:36, idx) .* reduce(33:36));
-    end
-    outputA(chan, i) = freq_5_to_15;
-    outputB(chan, i) = freq_20_to_25;
-    outputC(chan, i) = freq_75_to_115;
-    outputD(chan, i) = freq_125_to_160;
-    outputE(chan, i) = freq_160_to_175;
-  end
-  for i = fn_len - overlap + 1 : fn_len
-    outputA(chan, i) = freq_5_to_15;
-    outputB(chan, i) = freq_20_to_25;
-    outputC(chan, i) = freq_75_to_115;
-    outputD(chan, i) = freq_125_to_160;
-    outputE(chan, i) = freq_160_to_175;
+    freq_5_to_15    =  3.0 * mean(P( 2: 4, i - 1) .* reduce( 2: 4));
+    freq_20_to_25   =  5.5 * mean(P( 5: 6, i - 1) .* reduce( 5: 6));
+    freq_75_to_115  = 20.0 * mean(P(16:24, i - 1) .* reduce(16:24));
+    freq_125_to_160 = 29.5 * mean(P(26:33, i - 1) .* reduce(26:33));
+    freq_160_to_175 = 34.5 * mean(P(33:36, i - 1) .* reduce(33:36));
+
+    outputA(i, chan) = freq_5_to_15;
+    outputB(i, chan) = freq_20_to_25;
+    outputC(i, chan) = freq_75_to_115;
+    outputD(i, chan) = freq_125_to_160;
+    outputE(i, chan) = freq_160_to_175;
   end
 end
 
-if debug_output; fprintf('\n'); end
-
-output = [outputA; outputB; outputC; outputD; outputE];
+output = [outputA outputB outputC outputD outputE];
 end
