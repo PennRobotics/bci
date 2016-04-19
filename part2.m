5832fd6bd48f6635f1b8f586260db019f10c8080
Brian Wright <brwr@mit.edu>
2016-04-19 14:24:10 -0400

    Prepare to remove feature extraction mfile code and remove some hardcoding

--- part2.m
+++ part2.m
@@ -4,20 +4,25 @@ load subject1
 load subject2
 load subject3
 
+part_length1 = 50000;
+part_length2 = 50000;
+part_length3 = 50000;
+
+% TODO(brwr): Track down refs and change to 1, 2, 3
 part_length = 50000;
 
 % TODO(brwr): Expand this to all data and channels
-sub_1_train_part = Train_ECoG_1(1:part_length, 16:31);
-sub_2_train_part = Train_ECoG_2(1:part_length, 16:31);
-sub_3_train_part = Train_ECoG_3(1:part_length, 16:31);
+sub_1_train_part = Train_ECoG_1(1:part_length1, 16:31);
+sub_2_train_part = Train_ECoG_2(1:part_length1, 16:31);
+sub_3_train_part = Train_ECoG_3(1:part_length1, 16:31);
 
-sub_1_glove_part = Train_Glove_1(1:part_length, 1:5);
-sub_2_glove_part = Train_Glove_2(1:part_length, 1:5);
-sub_3_glove_part = Train_Glove_3(1:part_length, 1:5);
+sub_1_glove_part = Train_Glove_1(1:part_length2, 1:5);
+sub_2_glove_part = Train_Glove_2(1:part_length2, 1:5);
+sub_3_glove_part = Train_Glove_3(1:part_length2, 1:5);
 
-sub_1_test_part = Test_ECoG_1(1:part_length, 16:31);
-sub_2_test_part = Test_ECoG_2(1:part_length, 16:31);
-sub_3_test_part = Test_ECoG_3(1:part_length, 16:31);
+sub_1_test_part = Test_ECoG_1(1:part_length3, 16:31);
+sub_2_test_part = Test_ECoG_2(1:part_length3, 16:31);
+sub_3_test_part = Test_ECoG_3(1:part_length3, 16:31);
 
 clear T*
 
