721fb3a02332e53003a3c4319583ac48ed4bb722
Brian Wright <brwr@mit.edu>
2016-04-18 23:17:42 -0400

    Load all subject data

--- part1.m
+++ part1.m
@@ -36,9 +36,9 @@ if subject3
   session_3_test = IEEGSession('I521_A0014_D003', username, password_bin);
 end
 
-nr_1_train   = session_1_train.data.rawChannels(1).getNrSamples;
-nr_1_glove   = session_1_glove.data.rawChannels(1).getNrSamples;
-nr_1_test    = session_1_test .data.rawChannels(1).getNrSamples;
+nr_1_train   = session_1_train.data.rawChannels(1).getNrSamples + 1;
+nr_1_glove   = session_1_glove.data.rawChannels(1).getNrSamples + 1;
+nr_1_test    = session_1_test .data.rawChannels(1).getNrSamples + 1;
 nc_1_train   = length(session_1_train.data.rawChannels);
 nc_1_glove   = length(session_1_glove.data.rawChannels);
 nc_1_test    = length(session_1_test .data.rawChannels);
@@ -46,9 +46,9 @@ data_1_train = session_1_train.data.getvalues(1 : nr_1_train, 1 : nc_1_train);
 data_1_glove = session_1_glove.data.getvalues(1 : nr_1_glove, 1 : nc_1_glove);
 data_1_test  = session_1_test .data.getvalues(1 : nr_1_test , 1 : nc_1_test );
 
-nr_2_train   = session_2_train.data.rawChannels(1).getNrSamples;
-nr_2_glove   = session_2_glove.data.rawChannels(1).getNrSamples;
-nr_2_test    = session_2_test .data.rawChannels(1).getNrSamples;
+nr_2_train   = session_2_train.data.rawChannels(1).getNrSamples + 1;
+nr_2_glove   = session_2_glove.data.rawChannels(1).getNrSamples + 1;
+nr_2_test    = session_2_test .data.rawChannels(1).getNrSamples + 1;
 nc_2_train   = length(session_2_train.data.rawChannels);
 nc_2_glove   = length(session_2_glove.data.rawChannels);
 nc_2_test    = length(session_2_test .data.rawChannels);
@@ -56,9 +56,9 @@ data_2_train = session_2_train.data.getvalues(1 : nr_2_train, 1 : nc_2_train);
 data_2_glove = session_2_glove.data.getvalues(1 : nr_2_glove, 1 : nc_2_glove);
 data_2_test  = session_2_test .data.getvalues(1 : nr_2_test , 1 : nc_2_test );
 
-nr_3_train   = session_3_train.data.rawChannels(1).getNrSamples;
-nr_3_glove   = session_3_glove.data.rawChannels(1).getNrSamples;
-nr_3_test    = session_3_test .data.rawChannels(1).getNrSamples;
+nr_3_train   = session_3_train.data.rawChannels(1).getNrSamples + 1;
+nr_3_glove   = session_3_glove.data.rawChannels(1).getNrSamples + 1;
+nr_3_test    = session_3_test .data.rawChannels(1).getNrSamples + 1;
 nc_3_train   = length(session_3_train.data.rawChannels);
 nc_3_glove   = length(session_3_glove.data.rawChannels);
 nc_3_test    = length(session_3_test .data.rawChannels);
