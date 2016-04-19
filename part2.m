6643145ebff0ab4e333cd87524e8df9902cbe71b
Brian Wright <brwr@mit.edu>
2016-04-18 23:15:44 -0400

    Partition data for testing algorithms

---
+++ part2.m
@@ -0,0 +1,19 @@
+clear all
+
+load subject1
+load subject2
+load subject3
+
+sub_1_train_part = Train_ECoG_1(1:50000, 16:31);
+sub_2_train_part = Train_ECoG_2(1:50000, 16:31);
+sub_3_train_part = Train_ECoG_3(1:50000, 16:31);
+
+sub_1_glove_part = Train_Glove_1(1:50000, 1:5);
+sub_2_glove_part = Train_Glove_2(1:50000, 1:5);
+sub_3_glove_part = Train_Glove_3(1:50000, 1:5);
+
+sub_1_test_part = Test_ECoG_1(1:50000, 16:31);
+sub_2_test_part = Test_ECoG_2(1:50000, 16:31);
+sub_3_test_part = Test_ECoG_3(1:50000, 16:31);
+
+clear T*
