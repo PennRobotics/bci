5431d299b0be472e9d1a833b18edf01ecebdceff
Brian Wright <brwr@mit.edu>
2016-04-18 21:23:04 -0400

    Change variable names, extract channel data from subject 1 training set

--- part1.m
+++ part1.m
@@ -3,25 +3,27 @@ password_bin = '~/Downloads/BCI/brw_ieeglogin.bin';
 
 % SUBJECT 1
 % Training ECoG
-data_1_train = IEEGSession('I521_A0012_D001', username, password_bin);
+session_1_train = IEEGSession('I521_A0012_D001', username, password_bin);
 % Training Data Glove
-data_1_glove = IEEGSession('I521_A0012_D002', username, password_bin);
+session_1_glove = IEEGSession('I521_A0012_D002', username, password_bin);
 % Testing ECoG
-data_1_test = IEEGSession('I521_A0012_D003', username, password_bin);
+session_1_test = IEEGSession('I521_A0012_D003', username, password_bin);
 
 % SUBJECT 2
 % Training ECoG
-data_2_train = IEEGSession('I521_A0013_D001', username, password_bin);
+session_2_train = IEEGSession('I521_A0013_D001', username, password_bin);
 % Training Data Glove
-data_2_glove = IEEGSession('I521_A0013_D002', username, password_bin);
+session_2_glove = IEEGSession('I521_A0013_D002', username, password_bin);
 % Testing ECoG
-data_2_test = IEEGSession('I521_A0013_D003', username, password_bin);
+session_2_test = IEEGSession('I521_A0013_D003', username, password_bin);
 
 % SUBJECT 3
 % Training ECoG
-data_3_train = IEEGSession('I521_A0014_D001', username, password_bin);
+session_3_train = IEEGSession('I521_A0014_D001', username, password_bin);
 % Training Data Glove
-data_3_glove = IEEGSession('I521_A0014_D002', username, password_bin);
+session_3_glove = IEEGSession('I521_A0014_D002', username, password_bin);
 % Testing ECoG
-data_3_test = IEEGSession('I521_A0014_D003', username, password_bin);
+session_3_test = IEEGSession('I521_A0014_D003', username, password_bin);
 
+% TODO(brwr): Replace hard-coded numbers with variables using IEEG object size
+data_1_train = session_1_train.data.getvalues(1 : 310000, 1 : 62);
