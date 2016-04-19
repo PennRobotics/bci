4e0e9dce3f7061cb4b166abf65bcb63e438b1f55
Brian Wright <brwr@mit.edu>
2016-04-18 22:43:04 -0400

    Load all subject data

--- part1.m
+++ part1.m
@@ -1,5 +1,10 @@
+clear all
 username = 'brwr';
 password_bin = '~/Downloads/BCI/brw_ieeglogin.bin';
+
+subject1 = true;
+subject2 = false;
+subject3 = false;
 
 % SUBJECT 1
 if subject1
@@ -32,6 +37,34 @@ if subject3
 end
 
 % TODO(brwr): Replace hard-coded numbers with variables using IEEG object size
-data_1_train = session_1_train.data.getvalues(1 : 310000, 1 : 62);
+nr_1_train   = session_1_train.data.rawChannels(1).getNrSamples;
+nr_1_glove   = session_1_glove.data.rawChannels(1).getNrSamples;
+nr_1_test    = session_1_test .data.rawChannels(1).getNrSamples;
+nc_1_train   = length(session_1_train.data.rawChannels);
+nc_1_glove   = length(session_1_glove.data.rawChannels);
+nc_1_test    = length(session_1_test .data.rawChannels);
+data_1_train = session_1_train.data.getvalues(1 : nr_1_train, 1 : nc_1_train);
+data_1_glove = session_1_glove.data.getvalues(1 : nr_1_glove, 1 : nc_1_glove);
+data_1_test  = session_1_test .data.getvalues(1 : nr_1_test , 1 : nc_1_test );
 
-plot(data_1_train)
+nr_2_train   = session_2_train.data.rawChannels(1).getNrSamples;
+nr_2_glove   = session_2_glove.data.rawChannels(1).getNrSamples;
+nr_2_test    = session_2_test .data.rawChannels(1).getNrSamples;
+nc_2_train   = length(session_2_train.data.rawChannels);
+nc_2_glove   = length(session_2_glove.data.rawChannels);
+nc_2_test    = length(session_2_test .data.rawChannels);
+data_2_train = session_2_train.data.getvalues(1 : nr_2_train, 1 : nc_2_train);
+data_2_glove = session_2_glove.data.getvalues(1 : nr_2_glove, 1 : nc_2_glove);
+data_2_test  = session_2_test .data.getvalues(1 : nr_2_test , 1 : nc_2_test );
+
+nr_3_train   = session_3_train.data.rawChannels(1).getNrSamples;
+nr_3_glove   = session_3_glove.data.rawChannels(1).getNrSamples;
+nr_3_test    = session_3_test .data.rawChannels(1).getNrSamples;
+nc_3_train   = length(session_3_train.data.rawChannels);
+nc_3_glove   = length(session_3_glove.data.rawChannels);
+nc_3_test    = length(session_3_test .data.rawChannels);
+data_3_train = session_3_train.data.getvalues(1 : nr_3_train, 1 : nc_3_train);
+data_3_glove = session_3_glove.data.getvalues(1 : nr_3_glove, 1 : nc_3_glove);
+data_3_test  = session_3_test .data.getvalues(1 : nr_3_test , 1 : nc_3_test );
+
+clear session* subject* nr* nc* username password_bin
