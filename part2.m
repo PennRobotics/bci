693835b02ffdceeab22b327a0ae041a0f8ee4137
Brian Wright <brwr@mit.edu>
2016-04-18 23:53:26 -0400

    Moving average for trimmed data

--- part2.m
+++ part2.m
@@ -17,3 +17,16 @@ sub_2_test_part = Test_ECoG_2(1:50000, 16:31);
 sub_3_test_part = Test_ECoG_3(1:50000, 16:31);
 
 clear T*
+
+sub_1_train_part_MA = MovingAverage(sub_1_train_part);
+sub_2_train_part_MA = MovingAverage(sub_2_train_part);
+sub_3_train_part_MA = MovingAverage(sub_3_train_part);
+
+sub_1_glove_part_MA = MovingAverage(sub_1_glove_part);
+sub_2_glove_part_MA = MovingAverage(sub_2_glove_part);
+sub_3_glove_part_MA = MovingAverage(sub_3_glove_part);
+
+sub_1_test_part_MA = MovingAverage(sub_1_test_part);
+sub_2_test_part_MA = MovingAverage(sub_2_test_part);
+sub_3_test_part_MA = MovingAverage(sub_3_test_part);
+
