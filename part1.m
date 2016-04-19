8ac35bd0022f6a5e2cc163319dffc165889912b8
Brian Wright <brwr@mit.edu>
2016-04-18 21:12:41 -0400

    Load datasets

---
+++ part1.m
@@ -0,0 +1,27 @@
+username = 'brwr'
+password_bin = '../brw_ieeglogin.bin'
+
+% SUBJECT 1
+% Training ECoG
+data_1_train = IEEGSession('I521_A0012_D001', username, password_bin);
+% Training Data Glove
+data_1_glove = IEEGSession('I521_A0012_D002', username, password_bin);
+% Testing ECoG
+data_1_test = IEEGSession('I521_A0012_D003', username, password_bin);
+
+% SUBJECT 2
+% Training ECoG
+data_2_train = IEEGSession('I521_A0013_D001', username, password_bin);
+% Training Data Glove
+data_2_glove = IEEGSession('I521_A0013_D002', username, password_bin);
+% Testing ECoG
+data_2_test = IEEGSession('I521_A0013_D003', username, password_bin);
+
+% SUBJECT 3
+% Training ECoG
+data_3_train = IEEGSession('I521_A0014_D001', username, password_bin);
+% Training Data Glove
+data_3_glove = IEEGSession('I521_A0014_D002', username, password_bin);
+% Testing ECoG
+data_3_test = IEEGSession('I521_A0014_D003', username, password_bin);
+
