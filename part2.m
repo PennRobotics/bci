e69ec691bcb7765c4bb37b84cefbad095ffb4899
Brian Wright <brwr@mit.edu>
2016-04-19 15:44:37 -0400

    Prevent memory overflow

--- part2.m
+++ part2.m
@@ -4,12 +4,12 @@ load subject1
 load subject2
 load subject3
 
-part_length1 = 50000;
-part_length2 = 50000;
-part_length3 = 50000;
+% part_length1 = 50000;
+% part_length2 = 50000;
+% part_length3 = 50000;
 
 % TODO(brwr): Track down refs and change to 1, 2, 3
-part_length = 50000;
+% part_length = 50000;
 
 % TODO(brwr): Expand this to all data and channels
 % sub_1_train_part = Train_ECoG_1(1:part_length1, 16:31);
@@ -24,50 +24,42 @@ part_length = 50000;
 % sub_2_test_part = Test_ECoG_2(1:part_length3, 16:31);
 % sub_3_test_part = Test_ECoG_3(1:part_length3, 16:31);
 
-sub_1_train = Train_ECoG_1(:, :);
-sub_2_train = Train_ECoG_2(:, :);
-sub_3_train = Train_ECoG_3(:, :);
+sub_1_train_MA = MovingAverage(Train_ECoG_1); disp('1 Train MA')
+save('subject1a.mat', 'sub_1_train_MA'); clear s*
+sub_1_train_FD = MovingFreqDomain(Train_ECoG_1); disp('1 Train FD')
+save('subject1b.mat', 'sub_1_glove_MA'); clear s*
+sub_1_glove_MA = MovingAverage(Train_Glove_1); disp('1 Glove MA')
+save('subject1c.mat', 'sub_1_test_MA'); clear s*
+sub_1_glove_FD = MovingFreqDomain(Train_Glove_1); disp('1 Glove FD')
+save('subject1d.mat', 'sub_1_train_FD'); clear s*
+sub_1_test_MA = MovingAverage(Test_ECoG_1); disp('1 Test MA')
+save('subject1e.mat', 'sub_1_glove_FD'); clear s*
+sub_1_test_FD = MovingFreqDomain(Test_ECoG_1); disp('1 Test FD')
+save('subject1f.mat', 'sub_1_test_FD'); clear s*
 
-sub_1_glove = Train_Glove_1(:, :);
-sub_2_glove = Train_Glove_2(:, :);
-sub_3_glove = Train_Glove_3(:, :);
+sub_2_train_MA = MovingAverage(Train_ECoG_2); disp('2 Train MA')
+save('subject2a.mat', 'sub_2_train_MA'); clear s*
+sub_2_train_FD = MovingFreqDomain(Train_ECoG_2); disp('2 Train FD')
+save('subject2b.mat', 'sub_2_glove_MA'); clear s*
+sub_2_glove_MA = MovingAverage(Train_Glove_2); disp('2 Glove MA')
+save('subject2c.mat', 'sub_2_test_MA'); clear s*
+sub_2_glove_FD = MovingFreqDomain(Train_Glove_2); disp('2 Glove FD')
+save('subject2d.mat', 'sub_2_train_FD'); clear s*
+sub_2_test_MA = MovingAverage(Test_ECoG_2); disp('2 Test MA')
+save('subject2e.mat', 'sub_2_glove_FD'); clear s*
+sub_2_test_FD = MovingFreqDomain(Test_ECoG_2); disp('2 Test FD')
+save('subject2f.mat', 'sub_2_test_FD'); clear s*
 
-sub_1_test = Test_ECoG_1(:, :);
-sub_2_test = Test_ECoG_2(:, :);
-sub_3_test = Test_ECoG_3(:, :);
-
-clear T*
-
-sub_1_train_MA = MovingAverage(sub_1_train);
-sub_2_train_MA = MovingAverage(sub_2_train);
-sub_3_train_MA = MovingAverage(sub_3_train);
-
-sub_1_glove_MA = MovingAverage(sub_1_glove);
-sub_2_glove_MA = MovingAverage(sub_2_glove);
-sub_3_glove_MA = MovingAverage(sub_3_glove);
-
-sub_1_test_MA = MovingAverage(sub_1_test);
-sub_2_test_MA = MovingAverage(sub_2_test);
-sub_3_test_MA = MovingAverage(sub_3_test);
-
-sub_1_train_FD = MovingFreqDomain(sub_1_train);
-sub_2_train_FD = MovingFreqDomain(sub_2_train);
-sub_3_train_FD = MovingFreqDomain(sub_3_train);
-
-sub_1_glove_FD = MovingFreqDomain(sub_1_glove);
-sub_2_glove_FD = MovingFreqDomain(sub_2_glove);
-sub_3_glove_FD = MovingFreqDomain(sub_3_glove);
-
-sub_1_test_FD = MovingFreqDomain(sub_1_test);
-sub_2_test_FD = MovingFreqDomain(sub_2_test);
-sub_3_test_FD = MovingFreqDomain(sub_3_test);
-
-save('subject1features.mat', 'sub_1_train_MA', 'sub_1_glove_MA', 'sub_1_test_MA', ...
-                             'sub_1_train_FD', 'sub_1_glove_FD', 'sub_1_test_FD');
-
-save('subject2features.mat', 'sub_2_train_MA', 'sub_2_glove_MA', 'sub_2_test_MA', ...
-                             'sub_2_train_FD', 'sub_2_glove_FD', 'sub_2_test_FD');
-
-save('subject3features.mat', 'sub_3_train_MA', 'sub_3_glove_MA', 'sub_3_test_MA', ...
-                             'sub_3_train_FD', 'sub_3_glove_FD', 'sub_3_test_FD');
+sub_3_train_MA = MovingAverage(Train_ECoG_3); disp('3 Train MA')
+save('subject3a.mat', 'sub_3_train_MA'); clear s*
+sub_3_train_FD = MovingFreqDomain(Train_ECoG_3); disp('3 Train FD')
+save('subject3b.mat', 'sub_3_glove_MA'); clear s*
+sub_3_glove_MA = MovingAverage(Train_Glove_3); disp('3 Glove MA')
+save('subject3c.mat', 'sub_3_test_MA'); clear s*
+sub_3_glove_FD = MovingFreqDomain(Train_Glove_3); disp('3 Glove FD')
+save('subject3d.mat', 'sub_3_train_FD'); clear s*
+sub_3_test_MA = MovingAverage(Test_ECoG_3); disp('3 Test MA')
+save('subject3e.mat', 'sub_3_glove_FD'); clear s*
+sub_3_test_FD = MovingFreqDomain(Test_ECoG_3); disp('3 Test FD')
+save('subject3f.mat', 'sub_3_test_FD'); clear s*
 
