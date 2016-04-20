2cbd544752799ec6f463f91a93d217aafe936606
Brian Wright <brwr@mit.edu>
2016-04-20 03:33:28 -0400

    Show script progress (verbose), N = 4

--- part2.m
+++ part2.m
@@ -102,9 +102,11 @@ N = 4; % Number of delays
 sub_1_train_final(:, 55) = [];
 sub_1_test_final(:, 55) = [];
 
-disp('Generating Training R Matrices')
+disp('1/3  Generating Training R Matrices')
 R1_full = GenerateRMatrix(sub_1_train_final, N);
+disp('2/3')
 R2_full = GenerateRMatrix(sub_2_train_final, N);
+disp('3/3')
 R3_full = GenerateRMatrix(sub_3_train_final, N);
 
 R1 = R1_full(1 + N : end - N, :);
@@ -120,9 +122,11 @@ beta1 = (R1' * R1) \ R1' * Y1;
 beta2 = (R2' * R2) \ R2' * Y2;
 beta3 = (R3' * R3) \ R3' * Y3;
 
-disp('Generating Testing R Matrices')
+disp('1/3  Generating Testing R Matrices')
 R1_X = GenerateRMatrix(sub_1_test_final, N);
+disp('2/3')
 R2_X = GenerateRMatrix(sub_2_test_final, N);
+disp('3/3')
 R3_X = GenerateRMatrix(sub_3_test_final, N);
 
 X1 = R1_X(1 + N : end - N, :); 
