a4cd109531d19a6f09de80da03fd375d1f9c5e52
Brian Wright <brwr@mit.edu>
2016-04-20 03:25:43 -0400

    Try N=4 without channel 55 on subject 1 (fixed)

--- part2.m
+++ part2.m
@@ -101,7 +101,6 @@ N = 4; % Number of delays
 % TODO(brwr): Make sure this works properly!
 sub_1_train_final(:, 55) = [];
 sub_1_test_final(:, 55) = [];
-sub1_glove_decimated(:, 55) = [];
 
 disp('Generating Training R Matrices')
 R1_full = GenerateRMatrix(sub_1_train_final, N);
