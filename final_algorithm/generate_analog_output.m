c37d638ded19e515d94bf192a1c1f574743193ed
Archana <archr@seas.upenn.edu>
2016-05-02 21:05:58 -0400

    R matrix generating and analog output generating secions placed in respective files

--- final_algorithm/generate_analog_output.m
+++ final_algorithm/generate_analog_output.m
@@ -1,1 +1,30 @@
-TODO(brwr)
+%% GENERATE OUTPUT
+disp('Generate Output')
+Y1_hat = X1 * beta1;
+Y2_hat = X2 * beta2;
+Y3_hat = X3 * beta3;
+
+disp('Pad Outputs')
+Y1_Final = [repmat(Y1_hat(1, :),N,1); Y1_hat; repmat(Y1_hat(end, :),N,1)];
+Y2_Final = [repmat(Y2_hat(1, :),N,1); Y2_hat; repmat(Y2_hat(end, :),N,1)];
+Y3_Final = [repmat(Y3_hat(1, :),N,1); Y3_hat; repmat(Y3_hat(end, :),N,1)];
+
+L1 = size(Test_ECoG_1, 1);
+L2 = size(Test_ECoG_2, 1);
+L3 = size(Test_ECoG_3, 1);
+
+disp('Output PCHIP')
+ YY1 = pchip(linspace(0, L1, size(Y1_Final, 1)), Y1_Final', linspace(0, L1, L1))';
+ YY2 = pchip(linspace(0, L2, size(Y2_Final, 1)), Y2_Final', linspace(0, L2, L2))';
+ YY3 = pchip(linspace(0, L3, size(Y3_Final, 1)), Y3_Final', linspace(0, L3, L3))';
+
+predicted_dg{1} = YY1;
+predicted_dg{2} = YY2;
+predicted_dg{3} = YY3;
+
+disp('Saving axon_fired.mat')
+save('axon_fired.mat', 'predicted_dg');
+
+disp('Done.')
+disp(' ')
+disp(' ')
