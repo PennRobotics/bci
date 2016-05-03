2cbecef1c924aa99c7a02f36d5e38ad06a213a80
Brian Wright <brianwright@mail.usf.edu>
2016-05-02 22:16:18 -0400

    Clean up function

--- final_algorithm/FnGenerateRMatrix.m
+++ final_algorithm/FnGenerateRMatrix.m
@@ -1,16 +1,16 @@
 function R = GenerateRMatrix(input, N);
-M = size(input, 1) - N +1;
+M = size(input, 1) - N + 1;
 v = size(input, 2);
 
-%R-matrix
+% R matrix
 R = ones(M, (1+(N*v)));    %easier to make first column a column of ones
 r = zeros(M, (N*v));
 ij = 0;
 
-for i =1:M
-    r (i, :)=reshape(input((ij+1):(N + ij),:),1,[]);
-    ij = ij +1;
+for i = 1 : M
+    r(i, :) = reshape(input((ij + 1) : (N + ij), :), 1, []);
+    ij = ij + 1;
 end
-R(:, 2:end) = r;    %Response matrixend
-fprintf('\n\nMean in Gen Function = %f', mean(mean(R)));
+R(:, 2 : end) = r;    %Response matrixend
+fprintf('(GenerateRMatrix) Mean in Gen Function = %f\n', mean(mean(R)));
 end
